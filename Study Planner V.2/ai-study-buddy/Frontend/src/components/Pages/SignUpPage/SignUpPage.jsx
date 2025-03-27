9f0542d3793ec04e1af91c928f9696b8646b02a1
Zain Hatim <zain.h.machiyara@gmail.com>
2025-03-27 12:44:08 +0500

    Update SignUpPage.jsx

--- Study Planner V.2/ai-study-buddy/Frontend/src/components/Pages/SignUpPage/SignUpPage.jsx
+++ Study Planner V.2/ai-study-buddy/Frontend/src/components/Pages/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './SignUpPage.css';
 
 import user_icon from '../../Assets/person.png';
@@ -9,6 +9,12 @@ import website_logo_transparent from '../../Assets/website-logo-transparent.png'
 import back_arrow from '../../Assets/Back-Arrow.png'
 
 const SignUpPage = () => {
+
+    const navigate = useNavigate();
+
+    const submithandleclick = () => {
+        navigate('/');
+    };
 
     return (
         <div className='page-container'>
@@ -58,10 +64,8 @@ const SignUpPage = () => {
 
                 </div>
 
-                <div className='signup-container'>
-                    <div className='signupbutton' onClick={()=>{}}>
-                        Sign Up
-                    </div>
+                <div className='signup-container' onClick={submithandleclick}>
+                    Sign Up
                 </div>
             </div>
         </div>
